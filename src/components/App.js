f0dc4a6ed9837deb7ee42073b143fae945a31890
bcasadei <bill@billcasadei.com>
2017-05-01 12:28:01 -0600

    Timer is working

--- src/components/App.js
+++ src/components/App.js
@@ -2,84 +2,22 @@ import React, { Component } from 'react';
 import '../style/App.css';
 import Header from './Header';
 import TimeAdjuster from './TimeAdjuster';
+import Timer from './Timer';
 import Rounds from './Rounds';
 import ResetButton from './ResetButton';
 
 class App extends Component {
-  state = {
-    headerText: 'Focus Time',
-    roundLength: 25,
-    shortBreakLength: 5,
-    longBreakLength: 25,
-    currentRound: 1,
-    timerProgress: '25:00',
-    paused: true
-  }
-
-  componentDidMount() {
-    
-  }
-
-  componentWillUnmount() {
-
-  }
-
-  updateTimer(value) {
-    this.setState({
-      timerProgress: value
-    });
-  }
-
-  // pauseTimer(value) {
-  //   this.setState({ paused: {value} });
-  //   console.log(this.state.paused);
-  // }
-
-  startTimer(duration) {
-    let start = Date.now();
-    let difference,
-        minutes,
-        seconds;
-
-    this.pauseTimer(false);
-
-    const timer = () => {
-      // The 25 represents the duration argument
-      difference = (25 * 60) - ((Date.now() - start) / 1000 | 0);
-
-      minutes = (difference / 60) | 0;
-      seconds = (difference % 60) | 0;
-
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      console.log(`${minutes}:${seconds}`); 
-      this.updateTimer(`${minutes}:${seconds}`);
-      
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerText: 'Focus Time',
+      roundLength: 25,
+      shortBreakLength: 5,
+      longBreakLength: 25,
+      currentRound: 1
     };
-   
-    timer();
-    setInterval(timer, 1000);
-
-    if(this.state.paused) {
-        return;
-    }
   }
-
-  renderButton() {
-    if(this.state.paused) {
-      return (
-        <i className="material-icons md-80 md-green"
-        onClick={this.startTimer()}>
-        play_circle_outline</i>
-      );
-    }
-
-    return (
-      <i className="material-icons md-80 md-red"
-      onClick={this.pauseTimer(true)}>
-      pause_circle_outline</i>
-    );
-  }
+  
 
   render() {
     return (
@@ -98,15 +36,8 @@ class App extends Component {
             timerName="Long Break" />
         </div>
 
-        <div className="timer-container">
-          <div className="timer-active">
-            <div className="timer-circle">
-              <div className="timer-text">{this.state.timerProgress}</div>
-              {this.renderButton()}
-            </div>
-          </div> 
-        </div>
-
+        <Timer />
+        
         <Rounds currentRound={this.state.currentRound}/>
 
         <ResetButton />
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,73 +1,109 @@
 import React, { Component } from 'react';
 
-  updateTimer(value) {
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerProgress: '25:00',
+      timerMinutes: 25,
+      timerSeconds: 0,
+      paused: true
+    };
+  }
+  
+  // componentDidUpdate() {
+  //   this.timerID = setInterval(
+  //     () => this.updateTimer(),
+  //     1000
+  //   );
+  // }
+
+  // componentWillUnmount() {
+  //   clearInterval(this.timerID);
+  // }
+
+  // updateTimer() {
+  //   const start = this.state.startTime;
+
+  //   let difference = (25 * 60) - ((Date.now() - start) / 1000 | 0);
+  //   let minutes = (difference / 60) | 0;
+  //   let seconds = (difference % 60) | 0;
+
+  //   seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  //   console.log(`${minutes}:${seconds}`);
+
+  //   this.setState({
+  //     timerProgress: `${minutes}:${seconds}`
+  //   });
+  // }
+
+  // startTimer() {
+  //   this.setState({
+  //     startTime: Date.now(),
+  //     paused: false
+  //   });
+  // }
+  pauseTimer() {
     this.setState({
-      timerProgress: value
+      paused: true
     });
+
+    clearInterval(this.timerID);
   }
 
-  pauseTimer(value) {
-    this.setState({ paused: {value} });
-    console.log(this.state.paused);
-  }
-
-  startTimer(duration) {
+  startTimer() {
     let start = Date.now();
-    let difference,
-        minutes,
-        seconds;
+    let { timerMinutes, timerSeconds } = this.state;
 
-    this.pauseTimer(false);
+    this.setState({ paused: false });
 
     const timer = () => {
-      // The 25 represents the duration argument
-      difference = (25 * 60) - ((Date.now() - start) / 1000 | 0);
+      let difference = ((timerMinutes * 60) + timerSeconds) - ((Date.now() - start) / 1000 | 0);
 
-      minutes = (difference / 60) | 0;
-      seconds = (difference % 60) | 0;
+      let minutes = (difference / 60) | 0;
+      let seconds = (difference % 60) | 0;
 
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      console.log(`${minutes}:${seconds}`); 
-      this.updateTimer(`${minutes}:${seconds}`);
-      
+      this.setState({
+        timerMinutes: minutes,
+        timerSeconds: seconds
+      });
     };
    
     timer();
-    setInterval(timer, 1000);
-
-    // if(this.state.paused) {
-    //     return;
-    // }
+    this.timerID = setInterval(timer, 1000);
   }
 
   renderButton() {
     if(this.state.paused) {
       return (
         <i className="material-icons md-80 md-green"
-        onClick={this.startTimer()}>
+        onClick={() => this.startTimer()}>
         play_circle_outline</i>
       );
-    }
+    };
 
     return (
       <i className="material-icons md-80 md-red"
-      onClick={this.pauseTimer(true)}>
+      onClick={() => this.pauseTimer(true)}>
       pause_circle_outline</i>
     );
   }
 
-class Timer extends Component {
   render() {
-    return(
+    const {timerMinutes, timerSeconds } = this.state;
+
+    return (
       <div className="timer-container">
         <div className="timer-active">
           <div className="timer-circle">
-            <div className="timer-text">{timerProgress}</div>
-            {renderButton()}
+            <div className="timer-text">
+              {`${timerMinutes}:${timerSeconds < 10 ? "0" + timerSeconds : timerSeconds}`}
+            </div>
+            {this.renderButton()}
           </div>
         </div> 
-      </div>
+     </div>
     );
   }
 }
